ff102f490adc104c014beb816ed89c3f54fb4da7
slim_hamila <slim.hamila.webd@gmail.com>
2019-12-31 14:12:22 +0100

    installation of CORS, imported and .use in app.js

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
+import cors from "cors";
 require("dotenv-extended").load();
 
 import indexRouter from "./routes/index";
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
