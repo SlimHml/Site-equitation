5cc154924aebc090046663ca70ef0887e433e8bb
slim_hamila <slim.hamila.webd@gmail.com>
2020-01-06 11:00:22 +0100

    Models of Article and Commentaires modified

--- backend/server/models/article.js
+++ backend/server/models/article.js
@@ -1,9 +1,18 @@
-'use strict';
+"use strict";
 module.exports = (sequelize, DataTypes) => {
-  const Article = sequelize.define('Article', {
-    title: DataTypes.STRING
-  }, {});
+  const Article = sequelize.define(
+    "Article",
+    {
+      title: DataTypes.STRING,
+      allowNull: false
+    },
+    {}
+  );
   Article.associate = function(models) {
+    Article.hasMany(models.Commentaires, {
+      foreignKey: "articleId",
+      as: "commentaire"
+    });
     // associations can be defined here
   };
   return Article;
