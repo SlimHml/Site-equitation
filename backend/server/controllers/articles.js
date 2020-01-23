016eb922c8cb2dea258d6155865245c75cbc67af
slim_hamila <slim.hamila.webd@gmail.com>
2020-01-23 14:24:33 +0100

    squeletton of article controller

--- backend/server/controllers/articles.js
+++ backend/server/controllers/articles.js
@@ -8,16 +8,23 @@ const Articles = require("../models").Articles;
 // Corps
 
 module.exports = {
-    postArticle: function (req, res) {
+    createArticle: function (req, res) {
         // Params
         const title = req.body.title;
         const content = req.body.content;
         const likes = req.body.likes;
 
+        // Conditions
         if (title.length <= 4) {
             return (res.status(400).json({ error: "Le titre doit comporter au moins 5 lettres" }))
         } else if (content.length == null) {
             return (res.status(400).json({ error: "Le contenu de l'article ne peut être vide" }))
         } else return (res.status(500).json({ error: "Erreur 500, la page n'existe pas" }))
-    }
+    },
+    listArticle: function (req, res) {
+        // à faire
+    },
+    deleteArticle: function (req, res) {
+        // à faire
+    },
 }
