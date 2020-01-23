44a2bc3ff3d48df578af97f6073320184ddf8d94
slim_hamila <slim.hamila.webd@gmail.com>
2020-01-23 14:49:26 +0100

    reduced else if in article controller

--- backend/server/controllers/articles.js
+++ backend/server/controllers/articles.js
@@ -2,6 +2,7 @@
 const bcrypt = require("bcrypt");
 const jwtUtils = require("../utils/jwt.utils");
 const Articles = require("../models").Articles;
+const Users = require("../models").Users;
 
 // Constantes
 
@@ -21,11 +22,13 @@ module.exports = {
         let headerAutho = req.headers["authorization"];
         let userId = jwtUtils.getUserId(headerAutho);
 
-        // Conditions
+        // Conditions titres
         if (title.length < TITLE_LENGTH) {
             return (res.status(400).json({ error: "Le titre doit comporter au moins 5 lettres" }))
-        } else if (title == null) {
+        }
+        if (title == null) {
             return (res.status(400).json({ error: "Il faut un titre à l'article :)" }))
+            // Conditions contents
         } else if (content.length < CONTENT_LENGTH) {
             return (res.status(400).json({ error: "Il faut au moins 10 caractères dans l'article" }))
         } else if (content == null) {
@@ -33,7 +36,11 @@ module.exports = {
         } else {
             return (res.status(500).json({ error: "Erreur 500, la page n'existe pas" }))
         }
+        Users.findOne({
+            where: { id: userId }
+        })
     },
+
     listArticle: function (req, res) {
         // à faire
     },
