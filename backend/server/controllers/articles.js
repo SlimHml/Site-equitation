cacf5eb268f6a3602319a17f72114c99e1f523a8
slim_hamila <slim.hamila.webd@gmail.com>
2020-01-23 14:39:01 +0100

    added conditions about content length, new consts for title length and content length

--- backend/server/controllers/articles.js
+++ backend/server/controllers/articles.js
@@ -5,6 +5,9 @@ const Articles = require("../models").Articles;
 
 // Constantes
 
+const TITLE_LENGTH = 5;
+const CONTENT_LENGTH = 10;
+
 // Corps
 
 module.exports = {
@@ -19,11 +22,13 @@ module.exports = {
         let userId = jwtUtils.getUserId(headerAutho);
 
         // Conditions
-        if (title.length <= 4) {
+        if (title.length < TITLE_LENGTH) {
             return (res.status(400).json({ error: "Le titre doit comporter au moins 5 lettres" }))
         } else if (title == null) {
             return (res.status(400).json({ error: "Il faut un titre à l'article :)" }))
-        } else if (content.length == null) {
+        } else if (content.length < CONTENT_LENGTH) {
+            return (res.status(400).json({ error: "Il faut au moins 10 caractères dans l'article" }))
+        } else if (content == null) {
             return (res.status(400).json({ error: "Le contenu de l'article ne peut être vide" }))
         } else {
             return (res.status(500).json({ error: "Erreur 500, la page n'existe pas" }))
