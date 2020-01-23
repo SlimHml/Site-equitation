267586924884143e8c35d718ebcd9444e9b8d88b
slim_hamila <slim.hamila.webd@gmail.com>
2020-01-23 14:19:12 +0100

    Forgot a return statement

--- backend/server/controllers/articles.js
+++ backend/server/controllers/articles.js
@@ -14,10 +14,10 @@ module.exports = {
         const content = req.body.content;
         const likes = req.body.likes;
 
-        if (title.length <= 5) {
+        if (title.length <= 4) {
             return (res.status(400).json({ error: "Le titre doit comporter au moins 5 lettres" }))
         } else if (content.length == null) {
             return (res.status(400).json({ error: "Le contenu de l'article ne peut être vide" }))
-        } else (res.status(500).json({ error: "Erreur 500, la page n'existe pas" }))
+        } else return (res.status(500).json({ error: "Erreur 500, la page n'existe pas" }))
     }
 }
